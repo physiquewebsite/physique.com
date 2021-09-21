af3e0e0b78879cfafc49c1f87bd9694854ca886e
Charulyadav <84919243+Charulyadav@users.noreply.github.com>
2021-09-21 14:15:26 +0530

    Update Pscript.js

--- Pscript.js
+++ Pscript.js
@@ -1,3 +1,12 @@
+function myFunction() {
+  var x = document.getElementById("topMenu");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+}
+
 var slideIndex = 0;
             showSlides();
 
