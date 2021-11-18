8a2b3e367aeec7258061ba69b07d16c59e9dc496
ericaattal <90444662+ericaattal@users.noreply.github.com>
2021-11-18 12:32:09 +0530

    Add files via upload

---
+++ yscript.js
@@ -0,0 +1,118 @@
+
+var element=document.getElementById("image1");
+
+element.addEventListener("mouseover",zoom1,false);
+element.addEventListener("mouseout",zoom2,false);
+
+function zoom1()
+{
+
+element.style.width="700px";
+element.style.height="500px";
+
+}
+
+var element=document.getElementById("image2");
+
+element.addEventListener("mouseover",zoom1,false);
+element.addEventListener("mouseout",zoom2,false);
+
+function zoom1()
+{
+
+element.style.width="700px";
+element.style.height="500px";
+
+}
+
+var element=document.getElementById("image3");
+
+element.addEventListener("mouseover",zoom1,false);
+element.addEventListener("mouseout",zoom2,false);
+
+function zoom1()
+{
+
+element.style.width="700px";
+element.style.height="500px";
+
+}
+
+var element=document.getElementById("image4");
+
+element.addEventListener("mouseover",zoom1,false);
+element.addEventListener("mouseout",zoom2,false);
+
+function zoom1()
+{
+
+element.style.width="700px";
+element.style.height="500px";
+
+}
+
+var element=document.getElementById("image5");
+
+element.addEventListener("mouseover",zoom1,false);
+element.addEventListener("mouseout",zoom2,false);
+
+function zoom1()
+{
+
+element.style.width="700px";
+element.style.height="500px";
+
+}
+
+var element=document.getElementById("image6");
+
+element.addEventListener("mouseover",zoom1,false);
+element.addEventListener("mouseout",zoom2,false);
+
+function zoom1()
+{
+
+element.style.width="700px";
+element.style.height="500px";
+
+}
+
+var element=document.getElementById("image7");
+
+element.addEventListener("mouseover",zoom1,false);
+element.addEventListener("mouseout",zoom2,false);
+
+function zoom1()
+{
+
+element.style.width="700px";
+element.style.height="500px";
+
+}
+
+
+var element=document.getElementById("image8");
+
+element.addEventListener("mouseover",zoom1,false);
+element.addEventListener("mouseout",zoom2,false);
+
+function zoom1()
+{
+
+element.style.width="700px";
+element.style.height="500px";
+
+}
+
+var element=document.getElementById("image9");
+
+element.addEventListener("mouseover",zoom1,false);
+element.addEventListener("mouseout",zoom2,false);
+
+function zoom1()
+{
+
+element.style.width="700px";
+element.style.height="500px";
+
+}
