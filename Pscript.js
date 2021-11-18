ced0ec8846a3132af4d9e80493ca5ea4b789f743
ericaattal <90444662+ericaattal@users.noreply.github.com>
2021-11-18 12:26:10 +0530

    Update Pscript.js

--- Pscript.js
+++ Pscript.js
@@ -1,41 +1,5 @@
-function myFunction() {
-  var x = document.getElementById("topMenu");
-  if (x.className === "menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "menu";
-  }
-}
 
-var slideIndex = 0;
-            showSlides();
-
-            function showSlides() {
-              var i;
-              var slides = document.getElementsByClassName("mySlides");
-              var dots = document.getElementsByClassName("dot");
-              for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-              }
-              slideIndex++;
-              if (slideIndex > slides.length) {slideIndex = 1}
-              for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-              }
-              slides[slideIndex-1].style.display = "block";
-              dots[slideIndex-1].className += " active";
-              setTimeout(showSlides, 5000); // Change image every 5 seconds
-            }
-
-var modal = document.getElementById('id01');
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-        function sub1()
+function sub1()
         {
              var inputPswrd = document.getElementById("psw").value;
             var fixPswrd = "5030@cs";
@@ -46,7 +10,26 @@ window.onclick = function(event) {
                 alert("Wrong password! Login Failed.");
             }
         }
-$(function()
-{
-  $("#footer").load("Footer.html");
-});
+
+
+
+(function(){
+        var imgLen = document.getElementById('imgGallary');
+        var images = imgLen.getElementsByTagName('img');
+        var counter = 1;
+
+        if(counter <= images.length){
+            setInterval(function(){
+                images[0].src = images[counter].src;
+
+                counter++;
+
+                if(counter === images.length){
+                    counter = 1;
+                }
+            },4000);
+        }
+})();
+
+
+
